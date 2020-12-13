dc64ca700cdbab473be8cb120813094001459a3a
Ilham Nugroho <ilhamngrh97@gmail.com>
2020-12-14 00:46:03 +0800

    Create Upload Database using FilePond. Uninstall Multer and Path.

--- models/book.js
+++ models/book.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const path = require ('path')
-const coverImageBasePath = 'uploads/bookCovers'
+
+// const path = require ('path')
+// const coverImageBasePath = 'uploads/bookCovers'
+// SUDAH TIDAK DIPAKAI, KARENA PAKAI FilePond
 
 const bookSchema = new mongoose.Schema({
   title : {
@@ -24,7 +26,12 @@ const bookSchema = new mongoose.Schema({
     required: true,
     default: Date.now
   },
-  coverImageName : {
+  coverImage : {
+    type: Buffer,
+    required: true
+  },
+
+  coverImageType: {
     type: String,
     required: true
   },
@@ -35,11 +42,20 @@ const bookSchema = new mongoose.Schema({
   }
 });
 
+// bookSchema.virtual('coverImagePath').get(function() {
+//   if(this.coverImageName != null) {
+//     return path.join('/', coverImageBasePath, this.coverImageName)
+//   }
+// })
 bookSchema.virtual('coverImagePath').get(function() {
-  if(this.coverImageName != null) {
-    return path.join('/', coverImageBasePath, this.coverImageName)
-  }
+  // if(this.coverImage != null && this.coverImageType != null) {return 'data:${this.coverImageType}; charset=utf-8; base64, ${this.coverImage.toString('base64')}'}
+  if (this.coverImage != null && this.coverImageType != null) {
+         return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+     }
+
 })
 
+
 module.exports = mongoose.model('Book', bookSchema);
-module.exports.coverImageBasePath = coverImageBasePath;
+// module.exports.coverImageBasePath = coverImageBasePath;
+// SUDAH TIDAK DIPAKAI, PAKAI FilePond
